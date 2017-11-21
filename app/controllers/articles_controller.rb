2e8e5aee584585bd4903ebd6d7fccecbd16048c0
Lorenzo Del Castillo <l.delcastillo.97@gmail.com>
2017-11-21 15:23:37 +0100

    added article site with working markdown sintax

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,42 @@
+class ArticlesController < ApplicationController
+  before_action :load_article, only: [:show, :update, :edit, :destroy]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @article.update_attributes(article_params)
+    redirect_to articles_path
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    Article.create(article_params)
+    redirect_to articles_path
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def load_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+end
